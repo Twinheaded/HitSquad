51e229ddb7d2074c0577d984050021dc16832854
Dotman-Robotics <aaron_jt05@hotmail.com>
2025-04-01 11:50:55 +1100

    BFS and File System

--- classes.py
+++ classes.py
@@ -1,109 +1,82 @@
-class Node:
-    def __init__(self, state):
-        """Create a search tree Node, derived from a parent by an action."""
-        self.state = state
+import os
+import sys
+import argparse
+import search_algorithms
+from search_algorithms import breadth_first_search
+print(dir(search_algorithms))
 
-    def __repr__(self):
-        return "<Node {}>".format(self.state)
 
-    # This will be used for eliminating repeated states (e.g., in GBFS and A* search methods)
-    def __eq__(self, other):
-        return isinstance(other, Node) and self.state == other.state
+def File_Reader(file):
 
-    # The object will be referred to by its hash value instead of the object itself
-    def __hash__(self):
-        return hash(self.state)
+    f = open(file, "r")
+    print(f.readline())
+    metric = f.readline().strip()
+    nodes = {} # Nodes
+
+    while metric != "Edges:":
+        print(metric)
+        num = (metric[0]) #Strings
+        x = int(metric[4])  #Numeric
+        y = int(metric[6])  #Numeric
+        nodes[num] = (x,y)
+        metric = f.readline().strip()
+
+    metric = f.readline().strip()
+
+    edges = {} #Edges
+    graph = {} #Graph
+
+    while metric:
+        #print(metric)
+        start = metric[1] #Strings
+        stop = metric[3] #Strings
+        val = int(metric[7]) #Numeric
+        if start in graph:
+            graph[start].append(stop)
+        else:
+            graph[start] = [stop]
+        edges[(start,stop)] = val
+        metric = f.readline().strip()
+
+    f.readline()
+    origin = f.readline().strip() #Origin
+    f.readline()
+    destination = f.readline().strip() #Destinations
+    destination = [i.lstrip() for i in destination.split(";")] 
+    #print(destination)
+    #print(graph)
+
+    return nodes, edges, graph, origin, destination
+
+def CostFinder (node1, node2):
+    try:
+        key = (str(node1), str(node2))
+        cost = edges[key]
+        return cost
+    except:
+        return "Path Not Viable"
+
 
     
-class RouteGraph:
-    """Stores the paths between the Nodes (Edges) and their associated cost of traversal
 
-    Typical use:
-        routeGraph = RouteGraph()
-        routeGraph.connect(node1, node2, 5)
-        routeGraph.connect(node1, node3, 1)
-        print(routeGraph.get(node1))
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--file",
+        default="Assignment_2A\PathFinder_test.txt",
+        help="Specify File Location")
+    parser.add_argument(
+        "--searchalgo",
+        default="BFS",
+        help="Specific Scat Value."
+    )
+    args = parser.parse_args()
 
-    Attributes:
-        graph_dict: The mapping of each Node to its Edges. E.g., {<Node 1>: {<Node 2>: 5, <Node 3>: 1}}
-    """
+    
+    nodes, edges, graph, origin, destination = File_Reader(args.file)
+    #print(destination)
+    finalpaths = breadth_first_search(origin, destination, graph)
+    print(finalpaths)
 
-    def __init__(self):
-        self.graph_dict = {}
-
-    def connect(self, A, B, cost):
-        """Add a one-directional link from A to B and attach the cost of traversing from A to B"""
-        self.graph_dict.setdefault(A, {})[B] = cost
-
-    def get(self, a, b=None):
-        """Return a link cost or a dict of {node: cost} entries.
-        .get(a,b) returns the cost or None;
-        .get(a) returns a dict of {node: cost} entries, possibly {}."""
-        links = self.graph_dict.setdefault(a, {})
-        if b is None:
-            return links
-        else:
-            for key in links:
-                if key is b:
-                    return links[b]
-            raise Exception(f"Connection referenced that does not exist\nOption {a} -> {b}")
-
-    def nodes(self):
-        """Return a list of nodes in the graph."""
-        s1 = set([k for k in self.graph_dict.keys()])
-        s2 = set([k2 for v in self.graph_dict.values() for k2, v2 in v.items()])
-        nodes = s1.union(s2)
-        return list(nodes)
-
-
-class RouteFindingProblem():
-    """
-    The problem of searching a RouteGraph to traverse from a given Node to any one of the listed destination Nodes
-
-    Attributes:
-        graph_dict: The mapping of each Node to its Edges. E.g., {<Node 1>: {<Node 2>: 5, <Node 3>: 1}}
-        initial: A Node from which the search begins.
-        goal: A list of Nodes that the search will terminate at. E.g., [node1, node2]
-        graph: A RouteGraph which stores all of the Edges that connect the Nodes
-    """
-
-    def __init__(self, initial, goal, graph):
-        self.initial = initial
-        self.goal = goal
-        self.graph = graph
-
-    # Given a state 's':
-    # Actions(s) returns a set of actions/choices the agent can take when in state 's'. 
-    def actions(self, s):
-        """The current node can traverse to any other node as long as it is connected by an edge (in the correct
-        direction)"""
-        return self.graph.get(s)
-
-    # Given an action 'a' and state 's':
-    # Result(s,a) returns the state resulting from executing the action 'a' in state 's'.
-    def result(self, s, a):
-        """The result of going to a neighbor is just that neighbor. Make sure that the traversal is possible."""
-        if a in self.actions(s):
-            return a
-
-    # Checks if current state 's' is a goal state
-    def goal_test(self, s):
-        if isinstance(self.goal, list):
-            return s in self.goal
-
-    # Returns the cost of traversing from A to B
-    def path_cost(self, A, B, cost_so_far=0):
-        """cost_so_far stores the value of g(n) for search algorithms like A*"""
-        return cost_so_far + (self.graph.get(A, B) or np.inf)
-
-    def find_min_edge(self):
-        """Find minimum value of edges."""
-        m = np.inf
-        for d in self.graph.graph_dict.values():
-            local_min = min(d.values())
-            m = min(m, local_min)
-
-        return m
-
-    # def h(self, node):
-    #     # TODO: Implement an appropriate heuristic
+if __name__ == '__main__':
+    main(sys.argv)
--- search_algorithms.py
+++ search_algorithms.py
@@ -1,57 +1,44 @@
-# from search import nodes, problem from classes import *
-def depth_first_search(problem):
-    print('\nRunning DFS algorithm...\n')
+import sys
+import warnings
+import argparse
+import numpy as np
+import pandas as pd
+from queue import Queue
+from collections import deque
 
-    #######################################
-    # EXPLANATION OF THE 'problem' OBJECT #
-    #######################################
-    # The following code is for reference #
-    # only.                               #
-    #                                     #
-    # Nodes should not be assigned to     #
-    # variables this way...               #
-    #                                     #
-    # I've just written it like this so   #
-    # you can see clearly what each       #
-    # method of the 'problem' class does. #
-    #######################################
-
-    n1 = problem.graph.nodes()[0]
-    n2 = problem.graph.nodes()[1]
-    n3 = problem.graph.nodes()[2]
-    n4 = problem.graph.nodes()[3]
-    n5 = problem.graph.nodes()[4]
-
-    # (returns) a dictionary of {Nodes: costs}; all possible actions from Node 2
-    print(problem.actions(n2))
-
-    # (returns) a Node; the resulting state after attempting to traverse from Node 3 to Node 1
-    print(problem.result(n3, n1))
-
-    # (returns) a boolean; whether or not the current state Node 5 is a goal state
-    print(problem.goal_test(n5))
-
-    # (returns) an int; the cost of traversing from Node 2 to Node 1
-    print(problem.path_cost(n2, n1))
-
-    # (returns) an int; the cost of traversing from Node 2 to Node 1 + the cost spent so far
-    #           Useful for the A* algorithm
-    print(problem.path_cost(n2, n1, 14))
-
-    # (returns) an int; the lowest edge cost in the environment
-    print(problem.find_min_edge())
-
-def breadth_first_search():
+def depth_first_search ():
     raise NotImplementedError
 
-def greedy_best_first_search():
+def breadth_first_search (origin, destinations, graph): # Using Queue because of LIFO
+    finalpaths = {} # Dictionary containing all final paths selected for each destination in destination
+    
+    visited = [origin] # marks all visited nodes 
+    start_path = [origin] # Beginning Path, only contains origin
+    frontier = Queue() 
+    frontier.put(start_path) 
+    while not frontier.empty(): 
+        path = frontier.get() #Pulls First value
+        last_node = path[-1] #Checks last node from value
+        if last_node in destinations: #Checks last node
+                finalpaths[last_node] = path 
+                if len(finalpaths) == len(destinations):
+                    return finalpaths
+        for n in graph.get(last_node, []):  # For all possible neighbours
+            if n not in visited:
+                visited.append(n)
+                new_path = path + [n]
+                frontier.put(new_path) # adds to the frontier
+        
+    return finalpaths
+
+def astar_search():
     raise NotImplementedError
 
-def a_star_search():
+def greedy_first_best_search():
     raise NotImplementedError
 
-def custom_algorithm_1():
+def iterative_deepening_depth_first_search():
     raise NotImplementedError
 
-def custom_algorithm_2():
+def beam_search():
     raise NotImplementedError

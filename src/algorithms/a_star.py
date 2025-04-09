d202b98c0a94cced1523657889e30ec8a36f7b58
jack-marken <204086569+jack-marken1@users.noreply.github.com>
2025-04-10 02:05:03 +1000

    A* OOP

--- src/algorithms/a_star.py
+++ src/algorithms/a_star.py
@@ -7,31 +7,24 @@ class AS(SearchMethod):
         goal_test, get_actions = self.problem.goal_test, self.problem.get_actions # Methods
         h = self.distance_heuristic
 
-        ## TODO: Convert to OOP
-
-        ################
-        self.print_state(node, get_actions(node)) # <-- For debugging only
-        ################
-        return None
-
-
-# class GBFS(SearchMethod):
-#     def search(self):
-#         goal_test, get_actions = self.problem.goal_test, self.problem.get_actions # Methods
-#         h = self.distance_heuristic
-
-#         while self.frontier:
-#             node, path = self.frontier.pop()          # state - the current node
-#             if goal_test(node):
-#                 final_path = path + [node]
-#                 return node, len(self.explored), final_path
-#             self.explored.append(node)
-#             ## A list of connected nodes (actions) sorted by the shortest distance to the nearest destination
-#             actions = [node for node in sorted(get_actions(node).keys(), key=lambda x: h(x), reverse=True)]
-#             for a in actions:
-#                 if not a in self.explored:
-#                     self.frontier.append((a, path + [node]))
-#             ################
-#             self.print_state(node, get_actions(node)) # <-- For debugging only
-#             ################
-#         return None
+        while self.frontier:
+            node, path = self.frontier.pop()          # state - the current node
+            path = path + [node]
+            self.explored.append(node)
+            if goal_test(node):
+                self.result = node
+                self.final_path = path
+                return
+            ## A list of connected nodes (actions) sorted by the shortest distance to the nearest destination
+            actions = [
+                    a[0] for a in sorted(
+                        get_actions(node).items(),
+                        key=lambda x: self.problem.path_cost(node, x[0]) + h(x[0]),
+                        reverse=True)
+                    ]
+            for a in actions:
+                if not a in self.explored:
+                    self.frontier.append((a, path))
+            ################
+            # self.print_state(node, actions) # <-- For debugging only
+            ################

0fe112b3e19c8044912c0722b6c313586e690c6c
jack-marken <204086569+jack-marken1@users.noreply.github.com>
2025-04-10 00:35:11 +1000

    Add 'name' attribute

--- src/algorithms/bfs.py
+++ src/algorithms/bfs.py
@@ -1,6 +1,8 @@
 from .search_method import SearchMethod
 
 class BFS(SearchMethod):
+    name = "BFS"
+
     def search(self):
         goal_test, get_actions = self.problem.goal_test, self.problem.get_actions # Methods
 

9721d98188ec2eff57101748d1073525e31aa589
jack-marken <204086569+jack-marken1@users.noreply.github.com>
2025-04-09 23:34:59 +1000

    Update GBFS outputs

--- src/algorithms/gbfs.py
+++ src/algorithms/gbfs.py
@@ -2,32 +2,37 @@ from .search_method import SearchMethod
 
 # Philip / Jack
 # ============================================================
-# class GBFS(SearchMethod):
-#     def search(self):
-#         goal_test, get_actions = self.problem.goal_test, self.problem.get_actions # Methods
-#         h = self.distance_heuristic
+class GBFS(SearchMethod):
+    def __init__(self, problem):
+        super().__init__(problem)
+        self.name = "DFS"
 
-#         while self.frontier:
-#             node, path = self.frontier.pop()          # state - the current node
-#             if goal_test(node):
-#                 final_path = path + [node]
-#                 return node, len(self.explored), final_path
-#             self.explored.append(node)
-#             ## A list of connected nodes (actions) sorted by the shortest distance to the nearest destination
-#             actions = [node for node in sorted(get_actions(node).keys(), key=lambda x: h(x), reverse=True)]
-#             for a in actions:
-#                 if not a in self.explored:
-#                     self.frontier.append((a, path + [node]))
-#             ################
-#             self.print_state(node, get_actions(node)) # <-- For debugging only
-#             ################
-#         return None
+    def search(self):
+        goal_test, get_actions = self.problem.goal_test, self.problem.get_actions # Methods
+        h = self.distance_heuristic
+
+        while self.frontier:
+            node, path = self.frontier.pop()          # state - the current node
+            path = path + [node]
+            self.explored.append(node)
+            if goal_test(node):
+                self.result = node
+                self.final_path = path
+                return
+            ## A list of connected nodes (actions) sorted by the shortest distance to the nearest destination
+            actions = [node for node in sorted(get_actions(node).keys(), key=lambda x: h(x), reverse=True)]
+            for a in actions:
+                if not a in self.explored:
+                    self.frontier.append((a, path))
+            ################
+            # self.print_state(node, get_actions(node)) # <-- For debugging only
+            ################
 
 
 # Jordan
 # ============================================================
-import heapq;
-class GBFS(SearchMethod):
+# import heapq;
+# class GBFS(SearchMethod):
     ###Greedy Best-First Search (GBFS) using Euclidean distance heuristic.###
     
     # > These are defined in search_algorithm.py (parent class) - Jack
@@ -51,28 +56,28 @@ class GBFS(SearchMethod):
     #             min_dist = dist
     #     return min_dist
     
-    def search(self):
-        ###Runs GBFS and returns the paths to all goals.###
-        origin = self.problem.initial
-        heapq.heappush(self.frontier, (self.heuristic(origin), [origin]))
+    # def search(self):
+    #     ###Runs GBFS and returns the paths to all goals.###
+    #     origin = self.problem.initial
+    #     heapq.heappush(self.frontier, (self.heuristic(origin), [origin]))
         
-        while self.frontier:
-            _, path = heapq.heappop(self.frontier)
-            node = path[-1]
+    #     while self.frontier:
+    #         _, path = heapq.heappop(self.frontier)
+    #         node = path[-1]
             
-            if self.problem.goal_test(node):
-                self.final_paths[node] = path
-                if len(self.final_paths) == len(self.problem.goal):
-                    return self.final_paths
+    #         if self.problem.goal_test(node):
+    #             self.final_paths[node] = path
+    #             if len(self.final_paths) == len(self.problem.goal):
+    #                 return self.final_paths
             
-            if node not in self.explored:
-                self.explored.add(node)
-                for neighbor in self.problem.actions(node):
-                    if neighbor not in self.explored:
-                        new_path = path + [neighbor]
-                        heapq.heappush(
-                            self.frontier,
-                            (self._euclidean_heuristic(neighbor), new_path)
-                        )
+    #         if node not in self.explored:
+    #             self.explored.add(node)
+    #             for neighbor in self.problem.actions(node):
+    #                 if neighbor not in self.explored:
+    #                     new_path = path + [neighbor]
+    #                     heapq.heappush(
+    #                         self.frontier,
+    #                         (self._euclidean_heuristic(neighbor), new_path)
+    #                     )
         
-        return self.final_paths
+    #     return self.final_paths

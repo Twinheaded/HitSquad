c23c043dc49455b07e4ccb7188dcd4b4129b3dc9
jack-marken <jackmarken22@gmail.com>
2025-04-09 20:24:57 +1000

    Add self.name attribute to DFS

--- src/algorithms/dfs.py
+++ src/algorithms/dfs.py
@@ -3,6 +3,11 @@ from .search_method import SearchMethod
 # Jack
 # ============================================================
 class DFS(SearchMethod):
+    def __init__(self, problem):
+        super().__init__(problem)
+        self.name = "DFS"
+
+
     def search(self):
         goal_test, get_actions = self.problem.goal_test, self.problem.get_actions # Methods
 
@@ -13,6 +18,7 @@ class DFS(SearchMethod):
                 return node, len(self.explored), path
             self.explored.append(node)
             ## A list of connected nodes (actions) sorted by the shortest distance to the nearest destination
+            print(self.explored)
             actions = [node for node in reversed(get_actions(node).keys())]
             for a in actions:
                 if not a in self.explored:

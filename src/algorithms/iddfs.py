b976998c420043f8726ec6f54cdcc46b6289d638
jack-marken <204086569+jack-marken1@users.noreply.github.com>
2025-04-13 20:57:07 +1000

    Comments

--- src/algorithms/iddfs.py
+++ src/algorithms/iddfs.py
@@ -16,10 +16,9 @@ class IDDFS(SearchMethod):
                 return
             depth += 1
 
-    def depth_limited_dfs(self, depth_limit):
+    def depth_limited_dfs(self, depth_limit):   # also known as Depth Limited Search (DLS)
         depth = 0
         local_explored = []
-
         while self.frontier:
             node, path, depth = self.frontier.pop()
             path = path + [node]
@@ -29,19 +28,17 @@ class IDDFS(SearchMethod):
                 self.explored += [node]
                 self.final_path = path
                 return True
-            if depth > depth_limit:
-                self.frontier = [(self.problem.initial, [], 0)]
-                self.explored += local_explored
-                print(self.explored)
+            if depth > depth_limit:     
+                self.frontier = [(self.problem.initial, [], 0)] # Reset frontier to be ready for the next depth
+                self.explored += local_explored                 # self.explored will hold the paths of all DLS iterations
                 return False
-
             actions = [node for node in reversed(sorted(self.problem.get_actions(node).keys(), key=lambda x: x.node_id))]
             depth += 1
             for a in actions:
                 if not a in local_explored:
                     self.frontier.append((a, path, depth))
             ################
-            self.print_state(node, actions) # <-- For debugging only
+            # self.print_state(node, actions) # <-- For debugging only
             ################
         return False
 

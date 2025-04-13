70f287286ae4e3a741dc14ded78186151bd59b28
jack-marken <204086569+jack-marken1@users.noreply.github.com>
2025-04-13 11:58:35 +1000

    Restore from 'w' typo

--- src/algorithms/dfs.py
+++ src/algorithms/dfs.py
@@ -14,7 +14,7 @@ class DFS(SearchMethod):
             self.explored.append(node)
             if goal_test(node):
                 self.result = node
-                w
+                self.final_path = path
                 return
             ## A list of connected nodes (actions) sorted by the shortest distance to the nearest destination
             actions = [node for node in reversed(sorted(get_actions(node).keys(), key=lambda x: x.node_id))]

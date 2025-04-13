e2666c3f0aaaefc7710d7686cae665751f0857a7
jack-marken <204086569+jack-marken1@users.noreply.github.com>
2025-04-13 15:49:46 +1000

    Simplified A*

--- src/algorithms/a_star.py
+++ src/algorithms/a_star.py
@@ -5,7 +5,7 @@ class AS(SearchMethod):
 
     def search(self):
         goal_test, get_actions = self.problem.goal_test, self.problem.get_actions # Methods
-        h = self.distance_heuristic
+        h = self.problem.distance_heuristic
 
         while self.frontier:
             node, path = self.frontier.pop()          # state - the current node
@@ -16,12 +16,8 @@ class AS(SearchMethod):
                 self.final_path = path
                 return
             ## A list of connected nodes (actions) sorted by the shortest distance to the nearest destination
-            actions = [
-                    a[0] for a in sorted(
-                        get_actions(node).items(),
-                        key=lambda x: self.problem.path_cost(node, x[0]) + h(x[0]),
-                        reverse=True)
-                    ]
+            actions_sorted_by_id = [a for a in sorted(get_actions(node).keys(), key=lambda x: x.node_id, reverse=True)]
+            actions = [a for a in sorted(actions_sorted_by_id, key=lambda x: self.problem.path_cost(node, x) + h(x), reverse=True)]
             for a in actions:
                 if not a in self.explored:
                     self.frontier.append((a, path))

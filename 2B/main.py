0bf857a4331e24836e4d775ca98ec3a3257ac702
jack-marken <jackmarken9@gmail.com>
2025-05-19 20:42:51 +1000

    Convert DFS from 2A to 2B with SCATS sites instead of Nodes

--- 2B/main.py
+++ 2B/main.py
@@ -18,29 +18,31 @@ if __name__ == "__main__":
 
     problem = fp.create_problem('2000', '4043') # Arguments: origin, destination
 
-    print("\nSITES\n================")
-    print(problem.sites)
+    # print("\nSITES\n================")
+    # print(problem.sites)
 
-    print("\nINTERSECTIONS\n================")
-    for i in problem.intersections:
-        print(i)
+    # print("\nINTERSECTIONS\n================")
+    # for i in problem.intersections:
+    #     print(i)
 
-    print("\nLINKS (filtered to just site 4043)\n================")
-    for link in [l for l in problem.links if l.origin.scats_num == '4043']:
-        print(link)
+    # print("\nLINKS (filtered to just site 4043)\n================")
+    # for link in [l for l in problem.links if l.origin.scats_num == '4043']:
+    #     print(link)
 
-    print("\nTrafficProblem.get_actions('4043')\n================")
-    for action in problem.get_actions('4043'):
-        print(action)
+    # print("\nTrafficProblem.get_actions('4043')\n================")
+    # for action in problem.get_actions('4043'):
+    #     print(action)
 
-    print("\nTrafficProblem.goal_test('4043')\n================")
-    print(problem.goal_test('4043'))
+    # print("\nTrafficProblem.goal_test('4043')\n================")
+    # print(problem.goal_test('4043'))
 
     # TODO: Finish the TrafficProblem.distance_heuristic() method:
     # print("\nTrafficProblem.distance_heuristic('2000')\n================")
     # print(problem.distance_heuristic('2000'))
 
     # TODO: Make this code work:
-    # searchObj = DFS(problem)
-    # searchObj.search()
-    # print(searchObj.result)
+    searchObj = DFS(problem)
+    searchObj.search()
+    print(searchObj.result)
+    print(searchObj.final_path)
+    print(len(searchObj.final_path))

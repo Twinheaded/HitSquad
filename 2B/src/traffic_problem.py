2e1753a3bea39b940f17facb0ceff528a55d66cc
jack-marken <jackmarken9@gmail.com>
2025-05-23 10:23:22 +1000

    Restore lost code in traffic_problem.py after the phil_gui branch was merged

--- 2B/src/traffic_problem.py
+++ 2B/src/traffic_problem.py
@@ -4,11 +4,17 @@ import numpy as np
 import datetime
 
 class TrafficProblem():
+    """
+    The Traffic-Based Route Guidance Problem.
+
+    Contains all sites, intersections, and connecting links.
+    An origin and destination site are set upon initialisation.
+    """
     def __init__(self, sites, intersections, origin, destination, links, time=datetime.time(0,0), estimator=None):
         self.sites = sites # [<Site>, <Site>, ...] - All sites in the problem
         self.intersections = intersections # [<Intersection>, <Intersection>, ...] - All intersections in the problem
-        self.origin = origin # <Site> - first site of the search
-        self.destination = destination # <Site> - the final site of the search
+        self.origin = next(s for s in self.sites if s.scats_num == origin)    # <Site> - first site of the search
+        self.destination = next(s for s in self.sites if s.scats_num == destination)        # <Site> - the final site of the search
         self.links = links # [<Link>, <Link>, ...]
         self.time = time # the current time
         self.estimator = estimator  # TravelTimeEstimator instance
@@ -20,9 +26,11 @@ class TrafficProblem():
                 actions.append(self.get_site_by_intersection(l.destination))
         return actions
 
+    # Returns a bool: is site 's' the destination?
     def goal_test(self, s):
         return s == self.destination
 
+     # Returns the travel time of traversing from site 'a' to site 'b'
     def travel_time(self, a, b):
         """
         Calculate travel time from Site a to Site b using the estimator.
@@ -31,7 +39,29 @@ class TrafficProblem():
             raise Exception("No estimator assigned to TrafficProblem.")
         return self.estimator.travel_time(a, b, self.time)
 
-    def distance_heuristic(self, s):
-        dx = s.coordinates[0] - self.destination.coordinates[0]
-        dy = s.coordinates[1] - self.destination.coordinates[1]
-        return (dx**2 + dy**2)**0.5
+    # Computes the Euclidian distance of the closest intersections between site 's' and the destination
+     def distance_heuristic(self, s):
+        if self.goal_test(s):
+            return 0
+
+        min_dist = float('inf')
+        for site_i in s.intersections: # Intersections of site 's'
+            for dest_i in self.destination.intersections: # Intersections of the destination site
+                d_lat = site_i.coordinates[0] - dest_i.coordinates[0]
+                d_long = site_i.coordinates[1] - dest_i.coordinates[1]
+                dist = (d_lat**2 + d_long**2)**0.5  # Euclidean distance
+                if dist < min_dist:
+                    min_dist = dist
+        return min_dist
+
+    def get_site_by_scats(self, scats_num):
+        for site in self.sites:
+            if site.scats_num == scats_num:
+                return site
+        return None
+
+    def get_site_by_intersection(self, intersection):
+        for site in self.sites:
+            if intersection in site.intersections:
+                return site
+        return None
